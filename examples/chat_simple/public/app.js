3dc688215ff8a16594b7935ce1de56aa31e32599
andrewhenderson <andrew.m.henderson@gmail.com>
2016-12-01 11:29:35 -0800

    Chat Simple example

---
+++ examples/chat_simple/public/app.js
@@ -0,0 +1,84 @@
+// Marionette
+// ===================
+Marionette.Renderer.render = function(template, data, view) {
+  if (typeof template === 'function') {
+    return template.call(view, data);
+  } else {
+    return template;
+  }
+};
+
+// Views
+// ===================
+
+var MessageInput = MarionetteRedux.connect()(Marionette.View.extend({
+  className: 'ui input',
+  template: function () {
+    return "<input type='text'/>\
+      <button class='ui primary button' type='submit'>Submit</button>"
+  },
+  ui: {
+    messageInput: 'input'
+  },
+  events: {
+    'click button': 'handleSubmit'
+  },
+  handleSubmit: function () {
+    this.props.dispatch(addMessage(this.ui.messageInput.val()));
+    this.ui.messageInput.val('');
+  }
+}));
+
+var MessageView = MarionetteRedux.connect()(Marionette.View.extend({
+  className: 'comment',
+  template: function () {
+    return this.model.get('message')
+  },
+  events: {
+    click: 'handleClick'
+  },
+  handleClick: function() {
+    this.props.dispatch(deleteMessage(this.collection.indexOf(this.model)))
+  }
+}));
+
+var messagesMapStateToProps = function(state) {
+  return state;
+};
+var MessagesView = MarionetteRedux.connect(messagesMapStateToProps)(Marionette.CollectionView.extend({
+  childView: MessageView,
+  className: "ui comments",
+  componentDidReceiveProps: function(update) {
+    this.collection.set(update.messages);
+  }
+}));
+
+var Root = MarionetteRedux.connect()(Marionette.View.extend({
+  className: 'ui segment',
+  template: function() {
+    return "<div id='MessagesView'></div>\
+      <div id='MessageInput'></div>"
+  },
+  regions: {
+    MessagesView: '#MessagesView',
+    MessageInput: '#MessageInput'
+  },
+  onRender: function () {
+    this.showChildView('MessagesView', new MessagesView({
+      collection: new Backbone.Collection()
+    }));
+    this.showChildView('MessageInput', new MessageInput());
+  }
+}));
+
+// Application
+// ==========================
+var App = Marionette.Application.extend({
+  region: '#root-element',
+  onStart: function() {
+    this.showView(new Root());
+  }
+});
+
+var myApp = new App();
+myApp.start();
---
+++ examples/chat_simple/public/store.js
@@ -0,0 +1,39 @@
+// Action Creators
+// ===================
+function addMessage(message) {
+  return {
+    type: 'ADD_MESSAGE',
+    message: message
+  }
+}
+function deleteMessage(index) {
+  return {
+    type: 'DELETE_MESSAGE',
+    index: index
+  }
+}
+
+// Redux Reducers
+// ===================
+var reducers = Redux.combineReducers({
+  messages: function(_state, action) {
+
+    var state = _state ? JSON.parse(JSON.stringify(_state)) : [];
+
+    if (action.type === 'ADD_MESSAGE') {
+      return state.concat({
+        message: action.message
+      })
+    } else if (action.type === 'DELETE_MESSAGE') {
+      return _.join(state.slice(0, action.index), state.slice(action.index + 1, state.length))
+    } else {
+      return state
+    }
+  }
+});
+
+// Redux Store
+// ===================
+var compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : Redux.compose;
+var store = compose()(Redux.createStore)(reducers);
+window.store = store;
